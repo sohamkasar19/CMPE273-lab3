e3fba47704c05df6e4bbb8d11ebd4b7aac4c22f8
sohamkasar19 <sohamkasar19@gmail.com>
2022-03-31 11:37:07 -0700

    thunk + persist update

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import storage from "redux-persist/lib/storage";
+import allReducers from "./store/reducers/allReducers";
+import { applyMiddleware, compose, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+
+const middlewares = applyMiddleware(thunk);
+
+const store = createStore(
+  persistedReducer,
+  compose(
+    middlewares,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
