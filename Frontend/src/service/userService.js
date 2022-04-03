b14ffa70bd5a18dff71d1c564a29a303b7c3704b
sohamkasar19 <sohamkasar19@gmail.com>
2022-04-03 01:32:09 -0700

    token jwt

--- Frontend/src/service/userService.js
+++ Frontend/src/service/userService.js
@@ -7,7 +7,7 @@ export const userLogin = (data) => {
     axios.post(`${backend}/user/login`, { data }).then((res) => {
       if (res.data.status === "ok") {
         console.log(res.data.token);
-        // localStorage.setItem("token", res.data.token);
+        localStorage.setItem("token", res.data.token);
         res.data.user["token"] = res.data.token;
         return dispatch(userInfo(res.data.user));
       }
