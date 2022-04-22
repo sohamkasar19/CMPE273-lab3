9ce2e371d657dc1806ba95e272c3897d606f9b27
sohamkasar19 <sohamkasar19@gmail.com>
2022-04-21 23:56:47 -0700

    changed response topic

--- Backend/kafka/kafkarpc.js
+++ Backend/kafka/kafkarpc.js
@@ -52,7 +52,7 @@ KafkaRPC.prototype.makeRequest = function (topic_name, content, callback) {
         topic: topic_name,
         messages: JSON.stringify({
           correlationId: correlationId,
-          replyTo: "response_topic",
+          replyTo: "response-topic",
           data: content
         }),
         partition: 0
@@ -77,7 +77,7 @@ KafkaRPC.prototype.setupResponseQueue = function (producer, topic_name, next) {
   self = this;
 
   //subscribe to messages
-  var consumer = self.connection.getConsumer("response_topic");
+  var consumer = self.connection.getConsumer("response-topic");
   consumer.on("message", function (message) {
     console.log("msg received");
     var data = JSON.parse(message.value);
