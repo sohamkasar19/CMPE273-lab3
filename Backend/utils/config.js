a01b2835231fd3a0f35aa901201bf7735394eee2
sohamkasar19 <sohamkasar19@gmail.com>
2022-04-24 03:10:45 -0700

    deployment changes

--- Backend/utils/config.js
+++ Backend/utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT : 8080,
     ATLAS_URI : "mongodb+srv://dbuser:dbpassword@etsy-cluster.l1wsv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    FRONTEND : "http://3.87.5.83:3000",
+    FRONTEND : "http://18.212.199.247:3000",
     // FRONTEND : "http://localhost:3000",
     SECRET : "secret",
     AWS_BUCKET_NAME : "etsy-images-bucket",
--- Frontend/src/config/backend.js
+++ Frontend/src/config/backend.js
@@ -1,2 +1,2 @@
-export const backend = "http://3.87.5.83:8080"
+export const backend = "http://18.212.199.247:8080"
 // export const backend = "http://localhost:8080"
